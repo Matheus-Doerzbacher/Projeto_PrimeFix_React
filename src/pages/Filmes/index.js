f68730b080545c5172095f8eabb3b4f3099d8b84
Matheus Doerzbacher <Matheus.doer@gmail.com>
2023-02-13 13:07:19 -0300

    Meio da aula 43

--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
 
 
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "451007c8ae3c6db33c87b50352d233da",
+                    language: "pt-BR",
+                }
+            })
+            .then((response) => {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme Não Encontrado")
+            })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log("Componente foi desmontado")
+        }
+    },[])
+
+
     return(
         <div>
-            <h1>BEM VINDO A PÁGINA DETALHES DO FILME</h1>
+            <h1>Acessando filme {id}</h1>
         </div>
     )
 }
